0aae32023ef2dd726f7366ce95bf1c8d7e4eb023
Foster Stule <camodude5@yahoo.com>
2018-05-31 11:35:57 -0400

    created a rectangle class that mimics square class methods

---
+++ Geometry/Rectangles.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+	class Rectangles
+	{
+		public double lengthOfRec { get; set; }
+		public double widthOfRec { get; set; }
+
+		public double getAreaRec()
+		{
+			return lengthOfRec * widthOfRec;
+		}
+
+		public double getPerimRec()
+		{
+			return (2 * lengthOfRec) + (2 * widthOfRec);
+		}
+		public void printRec()
+		{
+			Console.WriteLine($"The width and length of the rectangle are {widthOfRec}, {lengthOfRec}. So the area is {getAreaRec()} and the perimeter {getPerimRec()}");
+		}
+	}
+}
